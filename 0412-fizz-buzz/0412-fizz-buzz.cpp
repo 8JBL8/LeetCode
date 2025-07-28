f3df8fb5aad3e294b8d518f23c4b33337577a01d
8JBL8 <jayliya2004@gmail.com>
2025-07-28 22:11:24 +0530

    Time: 2 ms (42.81%), Space: 11.8 MB (16.18%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        string s="";
+        for(int i=1; i<=n; i++){
+            int z= i%3;
+            int y= i%5;
+            if(i%5==0 && i%3==0) {
+                ans.push_back("FizzBuzz");
+            }
+            else if(i%5==0){
+                ans.push_back("Buzz");
+            }
+            else if(i%3==0){
+                ans.push_back("Fizz");
+            }
+            else{
+                s+=to_string(i);
+                ans.push_back(s);
+                s="";
+            }
+        }
+        return ans;
+    }
+};
