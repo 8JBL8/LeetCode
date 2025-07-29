eed2787465a2c9e22f59fbb3cce69e55863c03fd
8JBL8 <jayliya2004@gmail.com>
2025-07-29 19:57:24 +0530

    Time: 6 ms (3.14%), Space: 10.6 MB (5.3%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int getLucky(string s, int k) {
         map<int,char> con;
+
         con={{1,'a'},{2,'b'},{3,'c'},{4,'d'},{5,'e'},{6,'f'},{7,'g'},{8,'h'},{9,'i'},{10,'j'},{11,'k'},{12,'l'},{13,'m'},{14,'n'},{15,'o'},{16,'p'},{17,'q'},{18,'r'},{19,'s'},{20,'t'},{21,'u'},{22,'v'},{23,'w'},{24,'x'},{25,'y'},{26,'z'}};
 
         string ans="";
@@ -13,6 +14,7 @@ public:
                 }
             }
         }
+        
         while(k!=0){
             int w=0;
             for(int j=0; j<ans.size(); j++){
