8df3a2e6538d8ea047074d321238268956bd4f5f
8JBL8 <jayliya2004@gmail.com>
2025-07-30 19:08:15 +0530

    Time: 8 ms (46.96%), Space: 28.3 MB (98%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.cpp
@@ -4,6 +4,7 @@ public:
         int he=nums.size()/2;
         int count=0;
         int ans=0;
+        
         sort(nums.begin(),nums.end());
         for(int i=1; i<nums.size(); i++){
             if(nums[i-1]==nums[i] && count<=he){
