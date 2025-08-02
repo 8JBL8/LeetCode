2ddd474f2c864c832172be9de76cc5c8a0977be2
8JBL8 <jayliya2004@gmail.com>
2025-08-02 22:07:55 +0530

    Time: 1 ms (48.44%), Space: 10.3 MB (18.27%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans=""; 
+        for(int i=0; i<s.size(); i++){
+            if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9')){
+                char c=tolower(s[i]);
+                ans+=c;
+            }
+            else continue;
+        }
+        int l=ans.length()/2;
+            int low=0;
+            int high=ans.length()-1;
+            for(int i=0; i<l; i++){
+                if(ans[low]!=ans[high]){
+                    return false;
+                    break;
+                }
+                else{
+                    low++;
+                    high--;
+                }
+            }
+        return true;
+    }
+};
