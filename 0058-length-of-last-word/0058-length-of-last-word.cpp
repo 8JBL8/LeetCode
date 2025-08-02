9e548c989266d7bf01ff4b5b6ea31462a8617a08
8JBL8 <jayliya2004@gmail.com>
2025-08-02 21:26:27 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.23%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        s.push_back(' ');
-        vector<string> words;
-        string kk="";
+        int i = s.size() - 1;
+    
+        while (i >= 0 && s[i] == ' ') i--;
 
-        for(int i=0; i<s.size(); i++){
-            if(s[i]!=' '){
-                kk.push_back(s[i]);
-            }
-            else{
-                words.push_back(kk);
-                kk="";
-            }
-        }
+        int length = 0;
+    
+        while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
 
-        for(int i=0; i<words.size(); i++){
-            if(words[i].size()==0){
-                words.erase(words.begin()+i);
-                i--;
-            }
-        }
-        int d=words.size()-1;
-        int len=words[d].length();
-        return len;
+    return length;
+
     }
 };
