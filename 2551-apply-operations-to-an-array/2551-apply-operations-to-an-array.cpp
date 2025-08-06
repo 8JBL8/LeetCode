060e9056078590ebfdd79d7a50ddabfc8a0191e2
8JBL8 <jayliya2004@gmail.com>
2025-08-07 00:40:14 +0530

    Time: 0 ms (100%), Space: 13.2 MB (60.46%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]){
+                nums[i-1]=nums[i-1]*2;
+                nums[i]=0;
+            }
+            else continue;
+        }
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=0){
+               ans.push_back(nums[i]);
+            }
+        }
+        while(ans.size()<nums.size()){
+            ans.push_back(0);
+        }
+        return ans;
+    }
+};
