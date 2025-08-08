d0c5df154cd6a9d6dda68d0386fc07da8f7545b1
8JBL8 <jayliya2004@gmail.com>
2025-08-09 03:53:09 +0530

    Time: 99 ms (5.13%), Space: 317.4 MB (5.03%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -3,7 +3,7 @@ public:
     string reverseWords(string s) {
         vector<string> ans;
         string k="";
-        
+
         s.push_back(' ');
         for(int i=0; i<s.length(); i++){
             if(s[i]!=' '){
@@ -19,6 +19,7 @@ public:
             k=k+ans[i];
             k=k+' ';
         }
+        
         k.pop_back();
         return k;
     }
