301ad5f9cc184a65dd1d4781b5e0884bc4c8f2e1
8JBL8 <jayliya2004@gmail.com>
2025-08-14 23:28:30 +0530

    Time: 3 ms (6.37%), Space: 9.4 MB (28.13%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        vector<int> con;
+
+        for(int i=0; i<s.size(); i++){
+            int x=int(s[i]);
+            con.push_back(x);
+        }
+        vector<int> ans;
+
+        for(int i=1; i<con.size(); i++){
+            int z= abs(con[i-1]-con[i]);
+            ans.push_back(z);
+        }
+        int sum = accumulate(ans.begin(), ans.end(), 0);
+
+        return sum;
+    }
+};
